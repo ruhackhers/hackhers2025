bfa1ee8ac5566119a56bd53fcfb5ec86a88a55da
nataliechow <nchow234@gmail.com>
2024-12-11 07:00:56 +0200

    hackhers 2025 changes

---
+++ js/script.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const counters = document.querySelectorAll(".counter");
+  
+    const startCounting = (counter) => {
+      const target = +counter.getAttribute("data-target");
+      const prefix = counter.getAttribute("data-prefix") || ""; 
+      const speed = 50; 
+  
+      let count = 0;
+  
+      const updateCount = () => {
+        count += Math.ceil(target / speed);
+        if (count >= target) {
+          counter.textContent = `${prefix}${target}+`; 
+        } else {
+          counter.textContent = `${prefix}${count}`;
+          requestAnimationFrame(updateCount);
+        }
+      };
+  
+      updateCount();
+    };
+  
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            const counter = entry.target;
+            counter.style.opacity = 1; 
+            startCounting(counter);
+            observer.unobserve(counter); 
+          }
+        });
+      },
+      { threshold: 0.5 } 
+    );
+  
+    counters.forEach((counter) => observer.observe(counter));
+  });
+  
